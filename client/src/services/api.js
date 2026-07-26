e734433a9af7a8e029866ade953a057994fc9be0
YASHWANTH <yashwanthmudhiraj@gmail.com>
2026-07-26 16:37:50 +0530

    complete 1

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 api.interceptors.request.use((config) => {
